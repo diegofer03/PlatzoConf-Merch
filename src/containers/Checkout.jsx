27ec72fbdf6aa7622262230782e933bad31fe97f
unknown <diegoafanador10@gmail.com>
2021-06-16 01:06:37 +0800

    [add] react-helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import '../styles/components/Checkout.css'
 import {Link} from 'react-router-dom'
 import AppContext from '../context/AppContext';
+import { Helmet } from 'react-helmet';
 
 const Checkout = () => {
     const {state :{cart},removeFromCart} = useContext(AppContext)
@@ -17,32 +18,39 @@ const Checkout = () => {
     }
 
     return (
-        <div className="Checkout">
-            <div className="Checkout-content">
 
-                {cart.length > 0 ? <h3>Lista</h3> : <h3>Sin Pedidos</h3>}
-                {cart.map((item)=>(
-                    <div className="Checkout-item" key={item.cartId}>
-                        <div className="Checkout-element">
-                            <h4>{item.title}</h4>
-                            <span>${item.price}</span>
+        <React.Fragment>
+            <Helmet>
+                <title>Lista de pedidos - PlatziConf Merch </title>
+            </Helmet>
+            <div className="Checkout">
+                <div className="Checkout-content">
+
+                    {cart.length > 0 ? <h3>Lista</h3> : <h3>Sin Pedidos</h3>}
+                    {cart.map((item)=>(
+                        <div className="Checkout-item" key={item.cartId}>
+                            <div className="Checkout-element">
+                                <h4>{item.title}</h4>
+                                <span>${item.price}</span>
+                            </div>
+                            <button type='button' onClick={handleRemove(item.cartId)}>
+                                <i className="fas fa-trash-alt" title='Eliminar'></i>
+                            </button>
                         </div>
-                        <button type='button' onClick={handleRemove(item.cartId)}>
-                            <i className="fas fa-trash-alt" title='Eliminar'></i>
-                        </button>
-                    </div>
-                ))}
-            </div>
-            {cart.length>0&&(
-                <div className="Checkout-sidebar">
-                    <h3>{`Precio total: ${handlePlusTotal()}`}</h3>
-                    <Link to='/checkout/info'>
-                        <button type='button'>Continuar pedido</button>
-                    </Link>              
+                    ))}
                 </div>
-            )}
-            
-        </div>
+                {cart.length>0&&(
+                    <div className="Checkout-sidebar">
+                        <h3>{`Precio total: ${handlePlusTotal()}`}</h3>
+                        <Link to='/checkout/info'>
+                            <button type='button'>Continuar pedido</button>
+                        </Link>              
+                    </div>
+                )}
+                
+            </div>
+        </React.Fragment>
+        
     );
 }
 
